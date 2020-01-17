973763a86ae9acf3bf25b285412d7a6edb3fb532
Guillaume Lamanda <guillaume.lamanda@zenika.com>
2020-01-17 15:21:34 +0100

    ♻️ update <Nav>

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const links: { href: string; label: string; key: string }[] = [
-  { href: "https://zeit.co/now", label: "ZEIT" },
-  { href: "https://github.com/zeit/next.js", label: "GitHub" }
+  { href: "/posts", label: "Posts" }
 ].map((link: any) => {
   link.key = `nav-link-${link.href}-${link.label}`;
   return link;
