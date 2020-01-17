0edeec8c4a34e2a78fcebbb714d692bdc8ad09a7
Guillaume Lamanda <guillaume.lamanda@zenika.com>
2020-01-17 15:22:27 +0100

    ✨ add graphql api route

---
+++ pages/api/graphql.ts
@@ -0,0 +1,58 @@
+import {
+  ApolloServer,
+  makeExecutableSchema,
+  gql,
+  ApolloError
+} from "apollo-server-micro";
+import { Photon, User } from "@prisma/photon";
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+const typeDefs = gql`
+  type Post {
+    id: ID
+    title: String
+  }
+
+  type Query {
+    hello: String
+    posts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello",
+    posts: async (_: void, __: void, { photon }: Context) => {
+      console.log(photon);
+      return photon.posts({ where: { published: true } });
+    }
+  }
+};
+
+type Context = {
+  photon: Photon;
+  user: Partial<User> | undefined;
+};
+
+const context = (): Context => ({
+  photon: new Photon(),
+  user: undefined
+});
+
+const server = new ApolloServer({
+  cacheControl: true,
+  context,
+  schema: makeExecutableSchema({
+    typeDefs,
+    resolvers
+  })
+});
+
+export default server.createHandler({
+  path: "/api/graphql"
+});
